cafce08b628b68154724e6b89a6648037824e84b
Ihar Ratner <iratner@incomm.com>
2023-06-20 14:29:55 +0300

    add script.sh

---
+++ script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# The URL of the GitHub repository
+REPO_URL="https://github.com/hashicorp/terraform-provider-azurerm"
+
+# The URL of the Teams incoming webhook
+TEAMS_URL="<SET_YOUR_WEBHOOK>"
+
+# The file to store the last release tag
+LAST_RELEASE_FILE="last_release.txt"
+
+# Get the latest release tag from the GitHub API
+LATEST_RELEASE=$(curl -s https://api.github.com/repos/hashicorp/terraform-provider-azurerm/releases/latest | jq -r ".tag_name")
+
+# Check if the file exists and create it if not
+if [ ! -f "$LAST_RELEASE_FILE" ]; then
+    touch "$LAST_RELEASE_FILE"
+fi
+
+# Read the last release tag from the file
+LAST_RELEASE=$(cat "$LAST_RELEASE_FILE")
+
+# Compare the latest release tag with the last release tag
+if [ "$LATEST_RELEASE" != "$LAST_RELEASE" ]; then
+    echo "$LATEST_RELEASE"
+    # Update the file with the latest release tag
+    echo "$LATEST_RELEASE" > "$LAST_RELEASE_FILE"
+
+    # Format the JSON payload as a connector card for Microsoft 365 Groups
+    response=$(curl https://api.github.com/repos/hashicorp/terraform-provider-azurerm/releases/latest)
+
+    JSON_PAYLOAD=$(jq -c --arg repo_url "$REPO_URL" --arg latest_release "$LATEST_RELEASE" '{ "@type": "MessageCard", "@context": "http://schema.org/extensions", "themeColor": "0076D7", "summary": "Latest terraform azurerm provider release", "sections": [ { "activityTitle": "Latest terraform azurerm provider release", "facts": [ { "name": "Version", "value": .tag_name }, { "name": "Date", "value": .published_at }, { "name": "Changelog", "value": .body } ], "markdown": true } ] }' <<< $response)
+
+    # Post the JSON payload to the Teams incoming webhook URL
+    curl -H "Content-Type: application/json" -d "$JSON_PAYLOAD" "$TEAMS_URL"
+
+    # Push new release version
+    git add last_release.txt
+    git commit -m "Updating last_release.txt file with new $LATEST_RELEASE version"
+    git push
+fi
